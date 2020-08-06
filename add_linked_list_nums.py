dc855b0f35b1063582cd26d916975a50a84ce252
Benjamin Flamm <beflamm@microsoft.com>
2020-08-06 18:03:44 -0400

    update linked-list for edge cases

--- add_linked_list_nums.py
+++ add_linked_list_nums.py
@@ -1,24 +1,29 @@
 #this is a leetcode problem with the goal of adding 2 linked lists i.e. (2->4 + 5->6 = 7->0->1) 42+65 = 107
-# won't work when the addition of the first digit results in a remainder
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = None
 
 class Solution(object):
+        
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-
+        
         curr_node_l1 = l1
         curr_node_l2 = l2
         
         nodes_remain = True
         carry = 0
-        
         return_val = 0
         
         while(nodes_remain):
-            
             
             if curr_node_l1 != None and curr_node_l2 != None:
                 if carry == 1:
@@ -34,14 +39,23 @@ class Solution(object):
                 else:
                     curr_node_l1.val = li_sum
                     curr_node_l2.val = li_sum
-
-                curr_node_l1 = curr_node_l1.next
-                curr_node_l2 = curr_node_l2.next
+                
+                if curr_node_l1.next == None and curr_node_l2.next == None and carry == 1:
+                    temp = ListNode()
+                    curr_node_l1.next = temp
+                    curr_node_l1 = curr_node_l1.next
+                    curr_node_l1.val = carry
+                    nodes_remain = False
+            
+                else:
+                    curr_node_l1 = curr_node_l1.next
+                    curr_node_l2 = curr_node_l2.next
                 
                 
             elif curr_node_l1 == None and curr_node_l2 != None:
                 
                 return_val = 1
+                
                 if carry == 1:
                     li_sum = curr_node_l2.val + carry
                     carry = 0
@@ -53,9 +67,15 @@ class Solution(object):
                     curr_node_l2.val = li_sum % 10
                 else:
                     curr_node_l2.val = li_sum
-
-                curr_node_l2 = curr_node_l2.next
                 
+                if curr_node_l2.next == None and carry == 1:
+                    temp = ListNode()
+                    curr_node_l2.next = temp
+                    curr_node_l2 = curr_node_l2.next
+                    curr_node_l2.val = carry
+                    nodes_remain = False
+                else:    
+                    curr_node_l2 = curr_node_l2.next
                 
                     
             elif curr_node_l2 == None and curr_node_l1 != None:
@@ -71,17 +91,21 @@ class Solution(object):
                     curr_node_l1.val = li_sum % 10
                 else:
                     curr_node_l1.val = li_sum
-
-                curr_node_l1 = curr_node_l1.next
                 
-                
+                if curr_node_l1.next == None and carry == 1:
+                    temp = ListNode()
+                    curr_node_l1.next = temp
+                    curr_node_l1 = curr_node_l1.next
+                    curr_node_l1.val = carry
+                    nodes_remain = False
+                else:
+                    curr_node_l1 = curr_node_l1.next
                 
             else:
                 nodes_remain = False
-                
-            
-        if return_val == 0:
-            return l1
-        else:
+        
+        if return_val == 1:
             return l2
+        else:
+            return l1
         
